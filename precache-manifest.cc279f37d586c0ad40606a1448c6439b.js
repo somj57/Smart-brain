1cff0191c6e8268c9aefc1ba8b8caa42ed566ea1
somsom <som.jaiswal.sj@gmail.com>
2020-10-13 22:07:18 +0530

    Updates

---
+++ precache-manifest.cc279f37d586c0ad40606a1448c6439b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ce0ef9f59cf02ab8fc64b834b67894",
+    "url": "/Smart-brain/index.html"
+  },
+  {
+    "revision": "b03df83f4ba416ff873b",
+    "url": "/Smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dfc8fb3e37bb432fcf52",
+    "url": "/Smart-brain/static/css/main.e65294d4.chunk.css"
+  },
+  {
+    "revision": "b03df83f4ba416ff873b",
+    "url": "/Smart-brain/static/js/2.7e75a222.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Smart-brain/static/js/2.7e75a222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc8fb3e37bb432fcf52",
+    "url": "/Smart-brain/static/js/main.87c77f44.chunk.js"
+  },
+  {
+    "revision": "a74b445ced070c8f3e96",
+    "url": "/Smart-brain/static/js/runtime-main.5bfb2ad3.js"
+  }
+]);
