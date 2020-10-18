98180f10d0a2d8b0f94b6d1574e6979417195f81
somsom <som.jaiswal.sj@gmail.com>
2020-10-18 18:54:32 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -23,16 +23,34 @@ const initialState = {
     email: '',
     entries: 0,
     joined: ''
-  }
+  },
+  box1:{},
+  box2:{},
+  box3:{},
+  box4:{},
+  box5:{},
+  box6:{},
+  box7:{},
+  box8:{},
+  box9:{},
+  box11:{},
+  box12:{},
+  box13:{},
+  box14:{},
+  box15:{},
+  box16:{},
+  box17:{},
+  box18:{},
+  box19:{},
 }
 
 const particlesOption = {
   particles: {
 	        number: {
-	            value: 120
+	            value: 100
 	        },
 	        size: {
-	            value: 4
+	            value: 2
 	        }
 	    },
 	    interactivity: {
@@ -68,7 +86,7 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+   const clarifaiFace = data.region_info.bounding_box;
    const image = document.getElementById('inputimage');
    const width = Number(image.width);
    const height = Number(image.height);
@@ -83,6 +101,92 @@ class App extends Component {
  displayFaceBox = (box) => {
     this.setState({box: box});
   }
+displayFaceBox1 = (box1) => {
+     this.setState({box1: box1});
+   }
+   displayFaceBox2 = (box2) => {
+  this.setState({
+    box2: box2
+  });
+}
+displayFaceBox3 = (box3) => {
+  this.setState({
+    box3: box3
+  });
+}
+displayFaceBox4 = (box4) => {
+  this.setState({
+    box4: box4
+  });
+}
+displayFaceBox5 = (box5) => {
+  this.setState({
+    box5: box5
+  });
+}
+displayFaceBox6 = (box6) => {
+  this.setState({
+    box6: box6
+  });
+}
+displayFaceBox7 = (box7) => {
+  this.setState({
+    box7: box7
+  });
+}
+displayFaceBox8 = (box8) => {
+  this.setState({
+    box8: box8
+  });
+}
+displayFaceBox9 = (box9) => {
+  this.setState({
+    box9: box9
+  });
+}
+displayFaceBox11 = (box11) => {
+     this.setState({box11: box11});
+   }
+   displayFaceBox12 = (box12) => {
+  this.setState({
+    box12: box12
+  });
+}
+displayFaceBox13 = (box13) => {
+  this.setState({
+    box13: box13
+  });
+}
+displayFaceBox14 = (box14) => {
+  this.setState({
+    box14: box14
+  });
+}
+displayFaceBox15 = (box15) => {
+  this.setState({
+    box15: box15
+  });
+}
+displayFaceBox16 = (box16) => {
+  this.setState({
+    box16: box16
+  });
+}
+displayFaceBox17 = (box17) => {
+  this.setState({
+    box17: box17
+  });
+}
+displayFaceBox18 = (box18) => {
+  this.setState({
+    box18: box18
+  });
+}
+displayFaceBox19 = (box19) => {
+  this.setState({
+    box19: box19
+  });
+}
 
   onInputChange = (event) =>{
     this.setState({input: event.target.value});
@@ -90,6 +194,27 @@ class App extends Component {
 
   onButtonSubmit = () => {
       this.setState({imageUrl: this.state.input});
+      this.setState({
+        box:{},
+        box1:{},
+        box2:{},
+        box3:{},
+        box4:{},
+        box5:{},
+        box6:{},
+        box7:{},
+        box8:{},
+        box9:{},
+        box11:{},
+        box12:{},
+        box13:{},
+        box14:{},
+        box15:{},
+        box16:{},
+        box17:{},
+        box18:{},
+        box19:{},
+      });
         fetch('https://enigmatic-atoll-45476.herokuapp.com/imageurl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
@@ -114,7 +239,25 @@ class App extends Component {
               .catch(console.log)
 
           }
-          this.displayFaceBox(this.calculateFaceLocation(response))
+          this.displayFaceBox(this.calculateFaceLocation(response.outputs[0].data.regions[0]))
+          this.displayFaceBox1(this.calculateFaceLocation(response.outputs[0].data.regions[1]))
+          this.displayFaceBox2(this.calculateFaceLocation(response.outputs[0].data.regions[2]))
+          this.displayFaceBox3(this.calculateFaceLocation(response.outputs[0].data.regions[3]))
+          this.displayFaceBox4(this.calculateFaceLocation(response.outputs[0].data.regions[4]))
+          this.displayFaceBox5(this.calculateFaceLocation(response.outputs[0].data.regions[5]))
+          this.displayFaceBox6(this.calculateFaceLocation(response.outputs[0].data.regions[6]))
+          this.displayFaceBox7(this.calculateFaceLocation(response.outputs[0].data.regions[7]))
+          this.displayFaceBox8(this.calculateFaceLocation(response.outputs[0].data.regions[8]))
+          this.displayFaceBox9(this.calculateFaceLocation(response.outputs[0].data.regions[9]))
+          this.displayFaceBox11(this.calculateFaceLocation(response.outputs[0].data.regions[11]))
+          this.displayFaceBox12(this.calculateFaceLocation(response.outputs[0].data.regions[12]))
+          this.displayFaceBox13(this.calculateFaceLocation(response.outputs[0].data.regions[13]))
+          this.displayFaceBox14(this.calculateFaceLocation(response.outputs[0].data.regions[14]))
+          this.displayFaceBox15(this.calculateFaceLocation(response.outputs[0].data.regions[15]))
+          this.displayFaceBox16(this.calculateFaceLocation(response.outputs[0].data.regions[16]))
+          this.displayFaceBox17(this.calculateFaceLocation(response.outputs[0].data.regions[17]))
+          this.displayFaceBox18(this.calculateFaceLocation(response.outputs[0].data.regions[18]))
+          this.displayFaceBox19(this.calculateFaceLocation(response.outputs[0].data.regions[19]))
         })
         .catch(err => console.log(err));
     }
@@ -149,6 +292,24 @@ class App extends Component {
                     <FaceRecognition
                       imageUrl={imageUrl}
                       box={box}
+                      box1={this.state.box1}
+                      box2={this.state.box2}
+                      box3={this.state.box3}
+                      box4={this.state.box4}
+                      box5={this.state.box5}
+                      box6={this.state.box6}
+                      box7={this.state.box7}
+                      box8={this.state.box8}
+                      box9={this.state.box9}
+                      box11={this.state.box11}
+                      box12={this.state.box12}
+                      box13={this.state.box13}
+                      box14={this.state.box14}
+                      box15={this.state.box15}
+                      box16={this.state.box16}
+                      box17={this.state.box17}
+                      box18={this.state.box18}
+                      box19={this.state.box19}
                       />
                   </div>
                 : (
@@ -164,4 +325,3 @@ class App extends Component {
 }
 
 export default App;
-
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, box,box1,box2,box3,box4,box5,box6,box7,box8,box9,box11,box12,box13,box14,box15,box16,box17,box18,box19 }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto'/>
         <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        <div className='bounding-box' style={{top: box1.topRow, right: box1.rightCol, bottom: box1.bottomRow, left: box1.leftCol}}></div>
+        <div className='bounding-box' style={{top: box2.topRow, right: box2.rightCol, bottom: box2.bottomRow, left: box2.leftCol}}></div>
+        <div className='bounding-box' style={{top: box3.topRow, right: box3.rightCol, bottom: box3.bottomRow, left: box3.leftCol}}></div>
+        <div className='bounding-box' style={{top: box4.topRow, right: box4.rightCol, bottom: box4.bottomRow, left: box4.leftCol}}></div>
+        <div className='bounding-box' style={{top: box5.topRow, right: box5.rightCol, bottom: box5.bottomRow, left: box5.leftCol}}></div>
+        <div className='bounding-box' style={{top: box6.topRow, right: box6.rightCol, bottom: box6.bottomRow, left: box6.leftCol}}></div>
+        <div className='bounding-box' style={{top: box7.topRow, right: box7.rightCol, bottom: box7.bottomRow, left: box7.leftCol}}></div>
+        <div className='bounding-box' style={{top: box8.topRow, right: box8.rightCol, bottom: box8.bottomRow, left: box8.leftCol}}></div>
+        <div className='bounding-box' style={{top: box9.topRow, right: box9.rightCol, bottom: box9.bottomRow, left: box9.leftCol}}></div>
+        <div className='bounding-box' style={{top: box11.topRow, right: box11.rightCol, bottom: box11.bottomRow, left: box11.leftCol}}></div>
+        <div className='bounding-box' style={{top: box12.topRow, right: box12.rightCol, bottom: box12.bottomRow, left: box12.leftCol}}></div>
+        <div className='bounding-box' style={{top: box13.topRow, right: box13.rightCol, bottom: box13.bottomRow, left: box13.leftCol}}></div>
+        <div className='bounding-box' style={{top: box14.topRow, right: box14.rightCol, bottom: box14.bottomRow, left: box14.leftCol}}></div>
+        <div className='bounding-box' style={{top: box15.topRow, right: box15.rightCol, bottom: box15.bottomRow, left: box15.leftCol}}></div>
+        <div className='bounding-box' style={{top: box16.topRow, right: box16.rightCol, bottom: box16.bottomRow, left: box16.leftCol}}></div>
+        <div className='bounding-box' style={{top: box17.topRow, right: box17.rightCol, bottom: box17.bottomRow, left: box17.leftCol}}></div>
+        <div className='bounding-box' style={{top: box18.topRow, right: box18.rightCol, bottom: box18.bottomRow, left: box18.leftCol}}></div>
+        <div className='bounding-box' style={{top: box18.topRow, right: box19.rightCol, bottom: box19.bottomRow, left: box19.leftCol}}></div>
+1
       </div>
     </div>
   );
